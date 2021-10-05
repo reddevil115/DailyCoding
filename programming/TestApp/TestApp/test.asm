cfd25c9433e8414a27eb3f9600d1cddf5a5c6eb9
reddevil115 <58662212+reddevil115@users.noreply.github.com>
2021-10-05 22:47:24 +0900

    Change Directories Name
    Change Directories Name.
    * Change Korean Name to English Name.
    
    * Some codes are deleted

--- programming/TestApp/TestApp/test.asm
+++
@@ -1,8 +0,0 @@
-MOV $8% rsi	// %rsi에 8을 저장
-MOV $4% rdi	// %rdi에 4를 저장
-MOV $8 4(% rdi, % rsi, 8)		// M[72]에 8을 저장
-ADD % rsi 4(% rdi, % rsi, 8)		// M[72]에 16이 저장
-PRT 4(% rdi, % rsi, 8)	// 16이 출력
-ADD $4 % rdi	// %rdi에 8이 저장
-PRT % rdi	// 8이 출력 
-PRT % rsi	// 8이 출력
