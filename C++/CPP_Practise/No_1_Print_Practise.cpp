a0ab12cec16a498e5c61273d408c6c5969cfdcbb
reddevil115 <58662212+reddevil115@users.noreply.github.com>
2021-12-21 21:27:44 +0900

    Add CPP code
    Code for practise.

---
+++ C++/CPP_Practise/No_1_Print_Practise.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main (void)
+{
+    std :: cout << "Hello, CPP !! " << std :: endl;
+    std :: cout << "This is a my first CPP program." << std :: endl;
+
+    return 0;
+}
