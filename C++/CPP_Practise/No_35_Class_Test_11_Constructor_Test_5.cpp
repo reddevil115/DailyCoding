51f1a32332994040bc831169df125a76e8770154
reddevil115 <58662212+reddevil115@users.noreply.github.com>
2022-02-20 00:32:13 +0900

    Add CPP Code
    Add CPP Code for practise.

---
+++ C++/CPP_Practise/No_35_Class_Test_11_Constructor_Test_5.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+class Test
+{
+    int value = 0;
+
+public:
+    Test()
+    {
+        std :: cout << ">>> Class Test is activate..." << std :: endl;
+    }
+
+    ~Test()
+    {
+        std :: cout << ">>> ~ Class Test is activate..." << std :: endl;
+        std :: cout << ">>> Class Test is deactivate..." << std :: endl;
+    }
+
+};
+
+int main(void)
+{
+    std :: cout << "* Main() begin." << std :: endl;
+
+    Test *test = new Test[5];
+    delete [] test;
+
+    std ::  cout << "* Main() end." << std :: endl;
+
+    return 0;
+}
