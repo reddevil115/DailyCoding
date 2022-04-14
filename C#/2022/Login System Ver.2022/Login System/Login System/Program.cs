745d84e02ac543736760de8eb739e3ce8c8c1bd9
reddevil115 <58662212+reddevil115@users.noreply.github.com>
2022-04-15 00:28:16 +0900

    Update folader and Add Csharp Codes
    Update folader and Add Csharp Codes

---
+++ C#/2022/Login System Ver.2022/Login System/Login System/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Login_System
+{
+    class Program
+    {
+        private static int menuVal = 0;
+
+        static void Main(string[] args)
+        {
+            UISystem ui = new UISystem();
+            ui.Header();
+            ui.Menu_infor();
+
+            menuVal = ui.Get_Menu_Val();
+
+            Console.WriteLine("* Your Selection : " + menuVal);
+        }
+    }
+}
---
+++ C#/2022/Login System Ver.2022/Login System/Login System/UISystem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Login_System
+{
+    class UISystem
+    {
+        private int buffer = 0;
+
+        public void Menu_infor()
+        {
+            Console.WriteLine("************************************* MENU *****************************************");
+            Console.WriteLine("1. Login");
+            Console.WriteLine("2. Join");
+            Console.WriteLine("************************************************************************************\n");
+        }
+
+        public int Get_Menu_Val()
+        {
+            Console.Write("* Type number of menu : ");
+            buffer = int.Parse(Console.ReadLine());
+
+            return buffer;
+        }
+
+        public void Header()
+        {
+            Console.WriteLine(" < Login System > \n");
+        }
+
+        public void Footer()
+        {
+            Console.WriteLine("Program End.");
+            Console.WriteLine("The TEAM FIFTY SEVEN");
+        }
+    }
+}
