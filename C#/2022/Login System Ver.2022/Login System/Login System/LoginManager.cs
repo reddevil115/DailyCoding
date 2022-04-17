3d2a3910a7c0bbd289c3274ba458526eab17d25d
reddevil115 <58662212+reddevil115@users.noreply.github.com>
2022-04-18 00:20:04 +0900

    Update Code
    Update Code

---
+++ C#/2022/Login System Ver.2022/Login System/Login System/LoginManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Login_System
+{
+    internal class LoginManager
+    {
+        public void LoginValidator()
+        {
+            
+        }
+
+        private void iDchecker()
+        {
+
+        }
+
+        private void PWchecker()
+        {
+
+        }
+    }
+}
