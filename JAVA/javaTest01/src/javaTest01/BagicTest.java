bd642561782e29ccdd74e2b4dc7833a88390950f
LOUIS KIM <58662212+reddevil115@users.noreply.github.com>
2022-04-24 00:20:45 +0900

    Add JAVA Codes
    Add JAVA Codes

---
+++ JAVA/javaTest01/src/javaTest01/BagicTest.java
@@ -0,0 +1,23 @@
+package javaTest01;
+
+public class BagicTest 
+{
+	public static int sum(int n, int m)
+	{
+		return m + n;
+	}
+	
+	public static void main(String[] args)
+	{
+		int i = 50;
+		int s;
+		char a;
+		
+		s = sum(i,10);
+		a = '!';
+		
+		System.out.println(a);
+		System.out.println("Hello!");
+		System.out.println(s);
+	}
+}
