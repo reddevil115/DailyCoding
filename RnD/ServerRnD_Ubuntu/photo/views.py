acf16a5c211becf29aaeafa696de8ecd014867b7
LOUIS KIM <58662212+reddevil115@users.noreply.github.com>
2023-01-18 19:09:22 +0900

    [UPDATE] Update Serveer

--- RnD/ServerRnD_Ubuntu/photo/views.py
+++ RnD/ServerRnD_Ubuntu/photo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Photo
 
 # Create your views here.
 def photo_list(request):
-    return render(request, 'photo/photo_list.html', {})
+    photos = Photo.objects.all()
+    return render(request, 'photo/photo_list.html', {'photos' : photos})
