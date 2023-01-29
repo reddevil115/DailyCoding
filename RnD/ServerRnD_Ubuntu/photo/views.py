4805cc2220b6800eb5b5c0707ed296f6593b4c47
LOUIS KIM <58662212+reddevil115@users.noreply.github.com>
2023-01-29 11:42:39 +0900

    [Update] Add Photo_Detail in Server RnD
    [Update] Add Photo_Detail in Server RnD

--- RnD/ServerRnD_Ubuntu/photo/views.py
+++ RnD/ServerRnD_Ubuntu/photo/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Photo
 
 # Create your views here.
 def photo_list(request):
     photos = Photo.objects.all()
     return render(request, 'photo/photo_list.html', {'photos' : photos})
+
+def photo_detail(request, pk):
+    photo = get_object_or_404(Photo, pk = pk)
+    return render(request, 'photo/photo_detail.html', {'photo': photo})
