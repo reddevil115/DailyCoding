4595860d020bd6f91df35635725195c29de7d716
LOUIS KIM <58662212+reddevil115@users.noreply.github.com>
2023-02-07 00:01:37 +0900

    [UPDATE] url.py
    Add code at url.py

--- RnD/ServerRnD_Ubuntu/photo/urls.py
+++ RnD/ServerRnD_Ubuntu/photo/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.photo_list, name = 'photo_list'),
     path('photo/<int:pk>/', views.photo_detail, name = 'photo_detail'),
+    path('photo/new/', views.photo_post, name = 'photo_post'),
 ]
