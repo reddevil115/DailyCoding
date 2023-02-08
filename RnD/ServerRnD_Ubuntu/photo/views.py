a7832af15261615c403bf6b3c25430c9d7b6fffb
LOUIS KIM <58662212+reddevil115@users.noreply.github.com>
2023-02-09 00:02:50 +0900

    [UPDATE] ADD CODES
    Add code at views.py

--- RnD/ServerRnD_Ubuntu/photo/views.py
+++ RnD/ServerRnD_Ubuntu/photo/views.py
@@ -22,3 +22,17 @@ def photo_post(request):
         form = PhotoForm()
     
     return render(request, 'photo/photo_post.html', {'form': form})
+
+def photo_edit(request, pk):
+    photo = get_object_or_404(Photo, pk = pk)
+    if request.method == "POST":
+        form = PhotoForm(request.POST, instance=photo)
+        if form.is_valid():
+            photo = form.save(commit = False)
+            photo.save()
+
+            return redirect('photo_detail', pk=photo.pk)
+    else:
+        form = PhotoForm(instance = photo)
+    
+    return render(request,'photo/photo_post.html', {'form' : form})
