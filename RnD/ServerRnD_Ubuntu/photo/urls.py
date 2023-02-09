5f0e15920065b376c033066adf0e9d7ec9587d77
LOUIS KIM <58662212+reddevil115@users.noreply.github.com>
2023-02-10 00:57:06 +0900

    [UPDATE] Update Code
    Add Code at urls.py

--- RnD/ServerRnD_Ubuntu/photo/urls.py
+++ RnD/ServerRnD_Ubuntu/photo/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.photo_list, name = 'photo_list'),
     path('photo/<int:pk>/', views.photo_detail, name = 'photo_detail'),
     path('photo/new/', views.photo_post, name = 'photo_post'),
+    path('photo/<int:pk>/edit/',views.photo_edit,name='photo_edit'),
 ]
