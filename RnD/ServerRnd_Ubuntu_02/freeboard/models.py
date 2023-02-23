3f6364d4a7ccddc819f97c2a26765178bc6c5885
LOUIS KIM <58662212+reddevil115@users.noreply.github.com>
2023-02-24 00:02:42 +0900

    [SET] Model.py
    [SET] Model.py

--- RnD/ServerRnd_Ubuntu_02/freeboard/models.py
+++ RnD/ServerRnd_Ubuntu_02/freeboard/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Content(models.Model):
+    title = models.ChaarField(max_length=50)
+    author = models.ChaarField(max_length=10)
+    description = models.TextField()
