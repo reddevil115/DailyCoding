bff88084aa4437310320fccbe1fba5af3e844246
LOUIS KIM <58662212+reddevil115@users.noreply.github.com>
2023-02-26 00:02:31 +0900

    [EDIT] Fix Error
    Fix Error

--- RnD/ServerRnd_Ubuntu_02/freeboard/models.py
+++ RnD/ServerRnd_Ubuntu_02/freeboard/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Content(models.Model):
-    title = models.ChaarField(max_length=50)
-    author = models.ChaarField(max_length=10)
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=10)
     description = models.TextField()
